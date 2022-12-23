5518b69a75d2c5e3581a2eb596fdd3d2dd44fadc
yusramahivish <120807558+yusramahivish@users.noreply.github.com>
2022-12-23 18:50:35 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,12 @@
+public class Natural{
+    
+  public static void main(String args[])
+    {     
+      int i;
+	System.out.println ("The first 10 natural numbers are \n");
+	for (i=1;i<=10;i++)
+	{      
+		System.out.println (i);
+	}
+}
+}
